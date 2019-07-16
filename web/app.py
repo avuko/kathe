f01081a11d39d213dfdf6d2865a2f03344705896
avuko <iamavuko@gmail.com>
2019-07-16 12:43:36 +0200

    zoom/focus on left click added

--- web/app.py
+++ web/app.py
@@ -484,6 +484,16 @@ function getsetinfo(setinfodata){{
           // It works better if only right-click zooms
           // Graph.centerAt(node.x, node.y, 1000);
           // Graph.zoom(8, 2000);
+
+          // 3d zoom
+          // Aim at node from outside it
+          const distance = 70;
+          const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+          Graph.cameraPosition(
+            {{ x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }}, // new position
+            node, // lookAt ({{ x, y, z }})
+            3000  // ms transition duration
+          );
           }}
 
         }})

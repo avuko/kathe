95886752249928b145fb91bbf42f9311f80fca9b
avuko <iamavuko@gmail.com>
2022-05-03 20:39:00 +0200

    cleaned up for new build

--- fillit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# using db 13 for prod 
-# 13 = binaries
-db='13'
-
-./malpedia.py | while read line; do echo "${line}" && ./kathe-cli.py ${line} -r "${db}";done
--- secrets.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# WARNING: file force-added to git!
-kathe_add_endpoint = 'http://127.0.0.1/add'
-redis_password = 'redis'
